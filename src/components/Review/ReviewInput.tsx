f9c1eba6a779bba1dabb940d940b736d157d654d
Enzo Falone <enzofalone123@gmail.com>
2024-04-04 00:16:01 -0400

    refactored modal logic into hook, created rating field

--- src/components/Review/ReviewInput.tsx
+++ src/components/Review/ReviewInput.tsx
@@ -1,22 +1,16 @@
 "use client";
+
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useState } from "react";
-import ReviewModal from "./ReviewModal";
+import useReviewModal from "@/hooks/useReviewModal";
+import ReviewModal from "./ReviewModal/ReviewModal";
 
 const ReviewInput = () => {
-  const [isModalOpen, setModalOpen] = useState(false);
-
+  const { openModal, closeModal, isOpen } = useReviewModal();
+  console.log(isOpen);
   return (
     <>
-      {isModalOpen ? (
-        <div
-          className="w-screen h-screen absolute top-0 left-0 bg-black bg-opacity-35 transition-all"
-          onClick={() => setModalOpen(false)}
-        ></div>
-      ) : (
-        <></>
-      )}
+      <ReviewModal isOpen={isOpen} closeModal={closeModal} />
       <div className="flex w-full border-[var(--border)] bg-[#ffffff] dark:bg-[#3b3b3b] rounded-[6px]">
         <FontAwesomeIcon
           className={"w-5 text-[var(--icon)] ml-3 h-full my-auto"}
@@ -26,10 +20,9 @@ const ReviewInput = () => {
           className="w-full p-3 flex-grow rounded-[6px] outline-none"
           type="text"
           placeholder="Write a review..."
-          onClick={() => setModalOpen(true)}
+          onClick={openModal}
         />
       </div>
-      {isModalOpen && <ReviewModal setModalOpen={setModalOpen} />}
     </>
   );
 };
