2e6a2a7b7d7bb6116bde3727a3f426823f9a3905
lesterfernandez <5958256+lesterfernandez@users.noreply.github.com>
2024-04-12 13:53:50 -0400

    refactor script

--- seed_db.py
+++ seed_db.py
@@ -3,7 +3,6 @@ import urllib.request, json, psycopg
 appListUrl = "https://api.steampowered.com/ISteamApps/GetAppList/v2/";
 detailsUrl = "http://store.steampowered.com/api/appdetails?appids=";
 
-# insert an individual app into database
 def insertApp(cur, appid, title, synopsis, imageUrl):
     try: 
         cur.execute("""
@@ -14,7 +13,6 @@ def insertApp(cur, appid, title, synopsis, imageUrl):
     except Exception as e:
         print("Error insertApp: " + str(e))
 
-# return a hashmap with all stored appids
 def getStoredApps(cur):
     apps = dict()
     cur.execute("SELECT appid FROM apps")
@@ -23,34 +21,34 @@ def getStoredApps(cur):
         apps[int(record[0])] = True
     return apps
 
+def processApp(title, appId):
+    with urllib.request.urlopen(detailsUrl + str(appId)) as detailsUrlResponse:
+        appDetailsJson = json.load(detailsUrlResponse)
+        appDetails = appDetailsJson[str(appId)]
+        if "data" not in appDetails: return
+
+        appData = appDetails["data"]
+        imageUrl = appData["header_image"]
+        synopsis = appData["short_description"]
+
+        if imageUrl and synopsis: 
+            insertApp(cur, appId, title, synopsis, imageUrl)
+
 print("connecting to database...")
-with psycopg.connect("dbname=postgres user=postgres password=password port=5005") as conn:
-    conn.autocommit = True # save all insertions into database even if program crashes due to Error 429
+conn = psycopg.connect("postgresql://postgres:password@localhost:5005")
+conn.autocommit = True # save all insertions into database even if program crashes due to Error 429
 
-    with conn.cursor() as cur:
-        storedApps = getStoredApps(cur)
+with conn.cursor() as cur:
+    storedApps = getStoredApps(cur)
+    print("fetching all apps...") 
+    with urllib.request.urlopen(appListUrl) as appList:
+        appList = json.load(appList)
+        for app in appList["applist"]["apps"]:
+            title = app["name"]
+            appId = app["appid"]
+            if not title or appId in storedApps: continue
+            processApp(title, appId)
 
-        print("fetching all apps...") 
-        with urllib.request.urlopen(appListUrl) as appListUrlResponse:
-            appList = json.load(appListUrlResponse)
-
-            # fetch app details for every app
-            for app in appList["applist"]["apps"]:
-                title = app["name"]
-                appid = app["appid"]
-
-                if title and not appid in storedApps:
-                    with urllib.request.urlopen(detailsUrl + str(appid)) as detailsUrlResponse:
-                        appDetailsJson = json.load(detailsUrlResponse)
-                        appDetails = appDetailsJson[str(appid)]
-
-                        if "data" in appDetails:
-                            appDetailsData = appDetails["data"]
-
-                            imageUrl = appDetailsData["header_image"]
-                            synopsis = appDetailsData["short_description"]
-
-                            if imageUrl and synopsis:
-                                insertApp(cur, appid, title, synopsis, imageUrl)
-                        
+conn.close()
+                    
 print("finished fetching and inserting all possible apps!")
